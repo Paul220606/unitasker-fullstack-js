142454036b817c8450e06cdb4886d37d92428f91
hothanhhait <hothanhhait@student.unimelb.edu.au>
2026-06-22 18:47:32 +1000

    fix: fix the vercel origin for cors

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://unitasker-fullstack-js.vercel.app",
+    "https://unitasker-fullstack-mv71ipj48-unitasker.vercel.app/",
   ],
 }))
 
