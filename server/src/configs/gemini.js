6f7c049d53a01c89f3a223b5da5675b933b079d9
hothanhhait <hothanhhait@student.unimelb.edu.au>
2026-07-03 22:43:57 +1000

    test: test other AI models

--- server/src/configs/gemini.js
+++ server/src/configs/gemini.js
@@ -2,7 +2,7 @@ import {GoogleGenerativeAI} from "@google/generative-ai"
 import {GEMINIKEY} from "./env.js"
 
 const genAI = new GoogleGenerativeAI(GEMINIKEY)
-const model = genAI.getGenerativeModel({model: "gemini-2.0-flash"})
+const model = genAI.getGenerativeModel({model: "gemini-3.5-flash"})
 
 
 export {model}
